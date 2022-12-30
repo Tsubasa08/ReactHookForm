1db38fbdb57155235774671b8e8c112827904c5d
Tsubasa08 <46633705+Tsubasa08@users.noreply.github.com>
2022-12-30 16:12:01 +0900

    register 共存

--- ControllerForm.tsx
+++ ControllerForm.tsx
@@ -4,21 +4,37 @@ import { Input } from '@chakra-ui/react';
 import React = require('react');
 
 export const ControllerForm = () => {
-  const { handleSubmit, control } = useForm();
+  const {
+    handleSubmit,
+    control,
+    register,
+    formState: { errors },
+  } = useForm();
 
   return (
     <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <input
+        type="text"
+        {...register('first-name', {
+          required: 'This is required.',
+          maxLength: {
+            value: 2,
+            message: 'maxLength invalid',
+          },
+        })}
+      />
+      <ErrorMessage errors={errors} name="first-name" />
       <Controller
         control={control}
         rules={{ required: 'This is required.' }}
-        name="text-input"
+        name="last-name"
         render={({
           field: { onChange, onBlur, value, ref },
           formState: { errors },
         }) => (
           <div>
             <Input onChange={onChange} onBlur={onBlur} />
-            <ErrorMessage errors={errors} name="text-input" />
+            <ErrorMessage errors={errors} name="last-name" />
           </div>
         )}
       />
