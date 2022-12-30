fb6ebd6a9f587a8db67a6cd9b6c4ac4e63c30d53
Tsubasa08 <46633705+Tsubasa08@users.noreply.github.com>
2022-12-30 16:58:25 +0900

    nested component control

--- ControllerForm.tsx
+++ ControllerForm.tsx
@@ -1,45 +1,66 @@
-import { useForm, Controller } from 'react-hook-form';
+import {
+  useForm,
+  Controller,
+  FormProvider,
+  useFormContext,
+} from 'react-hook-form';
 import { ErrorMessage } from '@hookform/error-message';
 import { Input } from '@chakra-ui/react';
 import React = require('react');
 
 export const ControllerForm = () => {
-  const {
-    handleSubmit,
-    control,
-    register,
-    formState: { errors },
-  } = useForm();
+  const methods = useForm();
 
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
-      <input
-        type="text"
-        {...register('first-name', {
-          required: 'This is required.',
-          maxLength: {
-            value: 2,
-            message: 'maxLength invalid',
-          },
-        })}
-      />
-      <ErrorMessage errors={errors} name="first-name" />
-      <Controller
-        control={control}
-        rules={{ required: 'This is required.' }}
-        name="last-name"
-        render={({
-          field: { onChange, onBlur, value, ref },
-          formState: { errors },
-        }) => (
-          <div>
-            <Input onChange={onChange} onBlur={onBlur} />
-            <ErrorMessage errors={errors} name="last-name" />
-          </div>
-        )}
-      />
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit((data) => console.log(data))}>
+        <input
+          placeholder="first name"
+          {...methods.register('first-name', {
+            required: 'This is required.',
+            maxLength: {
+              value: 2,
+              message: 'maxLength invalid',
+            },
+          })}
+        />
+        <ErrorMessage errors={methods.formState.errors} name="first-name" />
+        <Controller
+          control={methods.control}
+          rules={{ required: 'This is required.' }}
+          name="last-name"
+          render={({ field: { onChange, onBlur }, formState: { errors } }) => (
+            <div>
+              <Input
+                onChange={onChange}
+                onBlur={onBlur}
+                placeholder="last name"
+              />
+              <ErrorMessage errors={errors} name="last-name" />
+            </div>
+          )}
+        />
+        <EmailInput />
 
-      <input type="submit" />
-    </form>
+        <input type="submit" />
+      </form>
+    </FormProvider>
+  );
+};
+
+const EmailInput = () => {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      control={control}
+      rules={{ required: 'This is required.' }}
+      name="email"
+      render={({ field: { onChange, onBlur }, formState: { errors } }) => (
+        <div>
+          <Input onChange={onChange} onBlur={onBlur} placeholder="email" />
+          <ErrorMessage errors={errors} name="email" />
+        </div>
+      )}
+    />
   );
 };
